25b0c1a839269199bcaeea39434eb16fbc8d436f
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 14:31:12 -0500

    fixed bug in comparisonOperators

--- comparisonOperators.py
+++ comparisonOperators.py
@@ -18,3 +18,12 @@ else:
 # we can do comparisons with the equals sign like js
 # == equal
 # != not equal
+
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
