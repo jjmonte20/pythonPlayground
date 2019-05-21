df8de90b2a6290a49f556a990035c770d28a2d46
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 13:28:55 -0500

    added a float exercise

--- ex2.py
+++ ex2.py
@@ -2,3 +2,8 @@ birth_year = input('Birth year: ')
 age = 2019 - int(birth_year)
 # input puts values as strings first, if we want to combine with ints, everything needs to be set as ints
 print(age)
+
+# Kilograms ex
+weight_lbs = input("weightt (lbs): ")
+weight_kg = float(weight_lbs) * 0.45
+print(weight_kg)
