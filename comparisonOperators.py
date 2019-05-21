86a6a8695604c382275fb01fbf239ba9fffadcc5
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 14:26:13 -0500

    added comparisions for if/else statements

---
+++ comparisonOperators.py
@@ -0,0 +1,20 @@
+temperature = 35
+
+if temperature > 30:
+    print("it's a hot day")
+else:
+    print("it's not a hot day")
+
+name = 'Julian'
+print(len(name))
+
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name must be a max of 50 characters")
+else:
+    print("Name is alright")
+
+# we can do comparisons with the equals sign like js
+# == equal
+# != not equal
