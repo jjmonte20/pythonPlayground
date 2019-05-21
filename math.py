d9e3cd62726199c20c5143caf2b699d113e7d4f9
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 13:57:50 -0500

    added basic math with python

---
+++ math.py
@@ -0,0 +1,30 @@
+# operators
+
+print(10 + 3)
+# addition
+print(10 - 3)
+# subtraction
+print(10 * 3)
+# multiplication
+print(10 / 3)
+# division
+print(10 % 3)
+# find remainder after division
+print(10 ** 3)
+# exponential
+
+# augmented divider operator
+x = 10
+x += 3
+# same as x = x + 3
+# can be done with -,/,*
+print(x)
+
+x = 10 + 3 * 2 ** 2
+# exponential happens before multiplication
+# multiplication happens before addition
+print(x)
+z = (10 + 3) * 2 ** 2
+print(z)
+# parenthesis happens before the other operators
+
