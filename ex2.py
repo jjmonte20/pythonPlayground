547ef82430fbf6aea290cffb9eb992e0b98dd7ce
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 13:36:23 -0500

    setting lines for showing dynamic printing

--- ex2.py
+++ ex2.py
@@ -4,6 +4,6 @@ age = 2019 - int(birth_year)
 print(age)
 
 # Kilograms ex
-weight_lbs = input("weightt (lbs): ")
+weight_lbs = input("Weight (lbs): ")
 weight_kg = float(weight_lbs) * 0.45
 print(weight_kg)
---
+++ multilineString.py
@@ -0,0 +1,20 @@
+course = '''
+Hey Julian,
+
+You're doing great.
+
+Julian
+'''
+# print(course)
+
+coarse = 'Python for Beginners'
+print(coarse)
+# prints the whole line
+print(coarse[1])
+# prints y
+print(coarse[-1])
+# prints s
+print(coarse[0:3])
+# prints Pyt
+print(coarse[0:-3])
+# prints Python for Beginn
