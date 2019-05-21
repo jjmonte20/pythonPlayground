4dbab23a8760af9740ee5c2a37c2ec2b25e40098
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 14:09:10 -0500

    added if/elif/else

---
+++ ifElse.py
@@ -0,0 +1,16 @@
+is_hot = False
+is_cold = False
+# setting a boolean
+
+if is_hot:
+    # only runs of the condition is true
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    # works like else if
+    print("It's a cool day")
+    print("Wear warm clothes")
+else:
+    # happens only of both statements are false
+    print("It's a lovely day")
+print("Enjoy your day")
