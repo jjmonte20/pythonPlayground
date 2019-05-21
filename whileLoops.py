08b093ecc533c9c243218fe3b9697df9564a9758
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 14:35:55 -0500

    made a while loop page

---
+++ whileLoops.py
@@ -0,0 +1,10 @@
+i = 1
+
+while i <= 5:
+    # loops while the condition is met, will stop when condition is false
+    print("*" * i)
+    # prints the star
+    i = i + 1
+#     This is important, need to make a failing condition, otherwise if will never stop the function
+print("Done")
+# this is not printed until while loop is done
