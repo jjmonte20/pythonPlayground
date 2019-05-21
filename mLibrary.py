4bbc6d473cd8ec3426164f03b9e4ede8167a73c6
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 14:03:08 -0500

    able to access a library outside of the initial python library

---
+++ mLibrary.py
@@ -0,0 +1,11 @@
+import math
+# module can be used to access the math library
+
+print(math.floor(2.1))
+# will always go to the bottom number
+
+x = 2.9
+print(round(x))
+# number will round to the nearest integer
+print(abs(-2.9))
+# will always have a positive number
