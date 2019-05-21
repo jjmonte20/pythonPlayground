55c18e28531f23d65c1e606068250339afb6fc17
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 13:40:01 -0500

    made a file with formatted and concatenated strings

---
+++ formattedStrings.py
@@ -0,0 +1,9 @@
+first = 'John'
+last = 'Smith'
+# John [Smith] is a coder
+message = first + " [" + last + "] is a coder"
+msg = f'{first} [{last}] is a coder'
+print(message)
+# concatenated string
+print(msg)
+# formatted string, this is more optimal for scaling
