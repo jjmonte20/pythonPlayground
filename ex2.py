83ed5412b0b5338c3f87c187bd9ab73000415c88
jjmonte20 <jjmonte20@gmail.com>
2019-05-21 13:26:01 -0500

    able to make expressions using inputs and integers

---
+++ ex2.py
@@ -0,0 +1,4 @@
+birth_year = input('Birth year: ')
+age = 2019 - int(birth_year)
+# input puts values as strings first, if we want to combine with ints, everything needs to be set as ints
+print(age)
--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,2 @@
 name = input('What is your name? ')
-# setting a value in the input changes the value of the variable
 print('Hi ' + name)
