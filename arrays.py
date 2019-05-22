f1feddc1e2137a0061e97674e49589847c70e7b1
jjmonte20 <jjmonte20@gmail.com>
2019-05-22 10:45:58 -0500

    added arrays

---
+++ arrays.py
@@ -0,0 +1,10 @@
+
+for item in range(50, 100, 2):
+    # range can be used to loop through numbers
+    print(item)
+
+prices = [10, 20, 30]
+total = 0
+for i in prices:
+    total += i
+print(f"Your total is ${total}")
