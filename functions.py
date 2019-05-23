c5003f6e06913d7a17582b3e86d74030e451c0fe
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 14:51:21 -0500

    added a return statement example

--- functions.py
+++ functions.py
@@ -7,4 +7,7 @@ print('Start')
 greet_user("Julian", "Montejano")
 # when a defined function has a parameter, it must be filled for the function to work
 greet_user("Rachel", "Douglas")
+greet_user(last_name="Montejano", first_name="Nicholas")
+# in this case keyword arguments can be used to improved the readability of the code, but every argument must be named
+# best practice is for sharing with others
 print('Finish')
