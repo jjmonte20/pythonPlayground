3181091385c479242c6656d032cbe6f6fbc10dcc
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 14:42:26 -0500

    added a second arguement to the function

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-def greet_user(name):
-    print(f'Hi {name}!')
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
     print('Welcome aboard')
 
 
 print('Start')
-greet_user("Julian")
+greet_user("Julian", "Montejano")
+# when a defined function has a parameter, it must be filled for the function to work
+greet_user("Rachel", "Douglas")
 print('Finish')
