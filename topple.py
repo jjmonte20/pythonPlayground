09aa744e438d950b54a4b823b5d2093de24eaf0f
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 14:17:12 -0500

    added topple and navigating a topple

---
+++ topple.py
@@ -0,0 +1,12 @@
+numbers = (1,2,3)
+# topples are not like arrays, they are not changeable, they are good for making sure a list cannot be changed
+
+# example of unpacking
+
+coordinates = (1, 2, 3)
+x, y, z = coordinates
+# same as
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+print(y)
