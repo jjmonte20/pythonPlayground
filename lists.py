5b8142cf6187200cc0ea06067b3db9ccb8f981ce
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 11:35:52 -0500

    did a number exercise with python for loop

--- lists.py
+++ lists.py
@@ -5,3 +5,9 @@ print(names[2:])
 # : can make an index that will go from the 3rd position to the end
 print(names[2:4])
 # going to a number will return between position 2 and 4, but not 4 itself
+numbers = [3,5,2,4,1,8]
+max = numbers[0]
+for num in numbers:
+    if num > max:
+        max = num
+print(max)
