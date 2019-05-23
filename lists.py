3ed3c86a4a0c9aab5941122ba165cfd8ab25e710
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 11:33:21 -0500

    learning lists tech when printing specific names

---
+++ lists.py
@@ -0,0 +1,7 @@
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+print(names[-1])
+# negative values always go to the end of the list
+print(names[2:])
+# : can make an index that will go from the 3rd position to the end
+print(names[2:4])
+# going to a number will return between position 2 and 4, but not 4 itself
