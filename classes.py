fb9fea9d580e591ca1918c09e228e1a341c8683c
jjmonte20 <jjmonte20@gmail.com>
2019-05-23 15:25:56 -0500

    Added a second example of a python class

--- classes.py
+++ classes.py
@@ -1,21 +1,29 @@
 # Classes can be used to define complex things
 class Point:
+    # init method makes a constructor
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def move(self):
         print('move')
-
 
     def draw(self):
         print('draw')
 
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
-print(point1.x)
+point = Point(10, 20)
+point.x = 11
+# can change object values after the fact
+print(point.x)
 # this creates the object of the class point
-point1.draw()
-# prints draw from point 1
 
+class Person:
+    def __init__(self, name):
+        self.name = name
 
-point2 = Point()
+    def talk(self):
+        print(f'Hello my name is {self.name}!')
 
+julian = Person("Julian")
+julian.talk()
