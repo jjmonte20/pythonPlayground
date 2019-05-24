8082153339edf711ca4576dc831f259fab1be006
jjmonte20 <jjmonte20@gmail.com>
2019-05-24 12:18:52 -0500

    using packages to import files from folders for functions

--- modules/findMax.py
+++ modules/findMax.py
@@ -2,6 +2,7 @@ from utils import find_max
 
 numbers = [10, 2, 34, 5, 67, 8]
 maximum = find_max(numbers)
+# do not change the meaning of things in a function by calling your variable as the same name from the function
 print(maximum)
 
 # when a term is defined in a previous function, it can be used to call a function
