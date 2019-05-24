777658ffcb85d3fa2fdb97e7d3b72f72018ec944
jjmonte20 <jjmonte20@gmail.com>
2019-05-24 12:06:00 -0500

    used import to grab a specific function in another file

---
+++ modules/findMax.py
@@ -0,0 +1,7 @@
+from utils import find_max
+
+numbers = [10, 2, 34, 5, 67, 8]
+maximum = find_max(numbers)
+print(maximum)
+
+# when a term is defined in a previous function, it can be used to call a function
